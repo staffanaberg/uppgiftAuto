17c72441a685fc0c542552e1d8a44dbf9d8eb0ea
Staffan Aberg <'staffan.aaberg@gmail.com'>
2021-03-16 10:59:01 +0100

    My first commit

---
+++ seleniumCucumber/src/test/java/stepDefinitions/StepDefinitions.java
@@ -0,0 +1,36 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinitions {
+
+
+
+		@Given("I have entered {int} into the calculator")
+		public void i_have_entered_into_the_calculator(Integer int1) {
+		   
+		}
+
+
+
+
+		@Given("I have also entered {int} into the calculator")
+		public void i_have_also_entered_into_the_calculator(Integer int1) {
+		    
+		}
+		@When("I press add")
+		public void i_press_add() {
+		  
+		}
+		@Then("the result should be {int} on the screen")
+		public void the_result_should_be_on_the_screen(Integer int1) {
+		    
+		}
+
+
+
+	
+	
+}
